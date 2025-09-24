2172f8143bd6905728de7acb6040bb5fbad2a6cf
TonmoySifat <tonmoysifatmd@gmail.com>
2025-09-24 16:41:13 +0600

    update form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,34 @@
-import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
+import type {Metadata} from "next";
+import {Bricolage_Grotesque} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
+    ClerkProvider
 } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
+    variable: "--font-bricolage",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
-  description: "Real-time AI Teaching Platform",
+    title: "Converso",
+    description: "Real-time AI Teaching Platform",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }}}>
-      <html lang="en">
-        <body className={`${bricolage.variable} antialiased`}>
-          <Navbar />
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider appearance={{variables: {colorPrimary: '#fe5933'}}}>
+            <html lang="en">
+            <body className={`${bricolage.variable} antialiased`}>
+            <Navbar/>
+            {children}
+            </body>
+            </html>
+        </ClerkProvider>
+    );
 }
