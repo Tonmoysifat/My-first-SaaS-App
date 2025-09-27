4acd5e98944a9bc5e75618bb0411d071d9e55fd8
TonmoySifat <tonmoysifatmd@gmail.com>
2025-09-27 15:34:29 +0600

    update the componenets page

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import {getAllCompanions} from "@/lib/actions/companions.action";
+import CompanionCards from "@/components/CompanionCards";
+import {getSubjectColor} from "@/lib/utils";
 
-const CompanionsPage = () => {
+const CompanionsPage = async ({searchParams}: SearchParams) => {
+
+    const filters = await searchParams
+
+    const subject = filters.subject ? filters.subject : ""
+    const topic = filters.topic ? filters.topic : ""
+
+    const companions = await getAllCompanions({subject, topic})
+
+    console.log(companions)
+
     return (
-        <div>
-            
-        </div>
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    search
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions?.map((companions) => (
+                    <CompanionCards key={companions.id} {...companions} color={getSubjectColor(companions.subject)}/>
+                ))}
+            </section>
+            Companions Library
+        </main>
     );
 };
 
